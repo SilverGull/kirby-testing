3fc868c56e7173f9a4dba8d679c3857ed9603427
SilverGull <peter.gull@gmail.com>
2020-03-29 22:16:19 +0200

    kirby added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentationComponentComponent } from './presentation-component/presentation-component.component';
+import { KirbyModule } from '@kirbydesign/designsystem';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PresentationComponentComponent } from './presentation-component/present
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KirbyModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
