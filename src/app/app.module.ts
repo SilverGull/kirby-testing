7b961e74e0c04a92e77836577218e36036f0b3d2
SilverGull <peter.gull@gmail.com>
2020-06-02 10:03:35 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KirbyModule } from '@kirbydesign/designsystem';
-import { OnPushComponent } from './on-push/on-push.component';
-import { DefaultComponent } from './default/default.component';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OnPushComponent,
-    DefaultComponent,
     ListComponent
   ],
   imports: [
