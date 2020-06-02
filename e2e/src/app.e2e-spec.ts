138777cd9a291da825012e4c023636646585f367
SilverGull <peter.gull@gmail.com>
2020-06-02 09:57:46 +0200

    rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('kirby-dropdown-bug app is running!');
+    expect(page.getTitleText()).toEqual('kirby-testing app is running!');
   });
 
   afterEach(async () => {
